889b911b4ee38371986cb48101911a90d1e42a9d
Wolfleader <james.morgan@iinet.net.au>
2021-04-29 20:26:31 +0800

    Textures!

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -8,6 +8,7 @@
 #include "VertexArray.h"
 #include "Shader.h"
 #include "VertexBufferLayout.h"
+#include "Texture.h"
 
 int main(void)
 {
@@ -42,10 +43,10 @@ int main(void)
 	{
 		float positions[] =
 		{
-			-0.5f, -0.5f, // 0
-			 0.5f, -0.5f, // 1
-			 0.5f,  0.5f, // 2
-			-0.5f,  0.5f, // 3
+			-0.5f, -0.5f, 0.0f, 0.0f, // 0
+			 0.5f, -0.5f, 1.0f, 0.0f, // 1
+			 0.5f,  0.5f, 1.0f, 1.0f,// 2
+			-0.5f,  0.5f, 0.0f, 1.0f// 3
 		};
 
 		unsigned int indices[] =
@@ -54,10 +55,14 @@ int main(void)
 			2,3,0
 		};
 
+		GLCall(glEnable(GL_BLEND));
+		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
 		VertexArray va;
-		VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+		VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 
 		VertexBufferLayout layout;
+		layout.Push<float>(2);
 		layout.Push<float>(2);
 		va.AddBuffer(vb, layout);
 
@@ -65,8 +70,11 @@ int main(void)
 
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
-
 		shader.SetUniform4f("u_Color", 0.2f, 0.4f, 0.8f, 1.0f);
+
+		Texture texture("res/textures/bear.png");
+		texture.Bind();
+		shader.SetUniform1i("u_Texture", 0);
 
 		va.UnBind();
 		vb.UnBind();
@@ -75,8 +83,6 @@ int main(void)
 
 		Renderer renderer;
 
-		float r = 0.0f;
-		float increment = 0.05f;
 		/* Loop until the user closes the window */
 		while (!glfwWindowShouldClose(window))
 		{
@@ -84,17 +90,8 @@ int main(void)
 			renderer.Clear();
 
 			shader.Bind();
-			shader.SetUniform4f("u_Color", r, 0.4f, 0.8f, 1.0f);
 
 			renderer.Draw(va, ib, shader);
-
-			if (r > 1.0f)
-				increment = -0.05f;
-			else if (r < 0.0f)
-				increment = 0.05f;
-
-			r += increment;
-
 			/* Swap front and back buffers */
 			glfwSwapBuffers(window);
 
